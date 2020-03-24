9b3e75d417520f348a05ac3f8d224b3ad6e13147
yhc <didguscjf95@naver.com>
2020-03-25 02:06:58 +0900

    update risk free asset modeling.py

---
+++ risk_free_asset_modeling.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def risk_free_value(init_val, r, t):
+    result = init_val * np.exp(r*t)
+    return result
+
+
+# Input as Value
+init_val = 100  # Initial Investment
+r = 0.02  # Risk-free interest
+t = 10  # Time to investment (year)
+
+# Output as Value
+v = risk_free_value(init_val, r, t)
+
+print(round(v, 2))
