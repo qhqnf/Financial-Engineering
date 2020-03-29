25ca8b59bf77f14effba867e371a8a120d213395
yhc <didguscjf95@naver.com>
2020-03-29 23:17:48 +0900

    option price 3D plotting
    plotly

--- option_pricing.py
+++ option_pricing.py
@@ -1,6 +1,10 @@
 # %%
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
 import numpy as np
 import scipy.stats as stat
+
+# black scholes option formula
 
 
 def europian_options(S, K, T, r, sigma, option_type):
@@ -19,4 +23,31 @@ print(europian_options(100, 100, 1, 0.02, 0.2, 'call'))
 
 print(europian_options(100, 100, 1, 0.02, 0.2, 'put'))
 
+# %%
+
+# import library
+init_notebook_mode(connected=True)
+
+# parameter
+K = 100
+r = 0.01
+sigma = 0.25
+
+# variables
+T = np.linspace(0, 1, 100)
+S = np.linspace(0, 200, 100)
+T, S = np.meshgrid(T, S)
+
+# output
+call_value = europian_options(S, K, T, r, sigma, 'call')
+put_value = europian_options(S, K, T, r, sigma, 'put')
+
+trace = go.Surface(x=T, y=S, z=call_value)
+data = [trace]
+layout = go.Layout(title='Option Price',
+                   scene={'xaxis': {'title': 'Maturity'}, 'yaxis': {'title': 'Spot Price'}, 'zaxis': {'title': 'Option Price'}})
+
+
+#fig = go.Figure(data=data, layout=layout)
+# iplot(fig)
 # %%
