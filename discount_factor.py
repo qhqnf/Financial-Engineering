8d62bdb7cf9a5facebea4e4fa9d7e61bda3eca84
yhc <didguscjf95@naver.com>
2020-04-30 00:30:15 +0900

    Update discount_factor.py

--- discount_factor.py
+++ discount_factor.py
@@ -2,19 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+bonds = []
+
 # %%
 with requests.get("https://tradingeconomics.com/united-states/repo-rate") as request:
     soup = BeautifulSoup(request.text, "html.parser")
     repo_table = soup.find("table")
-    repo_actual = table.find_all("tr")[1].find_all("td")[1].get_text()
-    print(repo_actual)
+    repo_actual = repo_table.find_all("tr")[1].find_all("td")[1].get_text()
+    repo_obj = {
+        "maturity": "O/N",
+        "yield": repo_actual
+    }
+    bonds.append(repo_obj)
 
 # %%
 request = requests.get(
     "https://tradingeconomics.com/united-states/government-bond-yield")
 soup = BeautifulSoup(request.text, "html.parser")
 bond_table = soup.find("table").find_all("tr")
-bonds = []
 for row_items in bond_table[1:]:
     maturity = row_items.find_all("td")[0].get_text(strip=True)
     y = row_items.find_all("td")[1].get_text(strip=True)
