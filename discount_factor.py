1bff01495094ae809e0ab932e198ccb738746487
yhc <didguscjf95@naver.com>
2020-05-02 23:35:04 +0900

    build calender

--- discount_factor.py
+++ discount_factor.py
@@ -43,11 +43,25 @@ period_array = np.array([ql.Period(1, ql.Days), ql.Period(1, ql.Months), ql.Peri
     2, ql.Years), ql.Period(3, ql.Years), ql.Period(5, ql.Years),
     ql.Period(7, ql.Years), ql.Period(10, ql.Years), ql.Period(20, ql.Years), ql.Period(30, ql.Years)])
 
-maturity_date_array = start_date + period_array
 
+# build calender
+maturity_date = []
+
+us_cal = ql.UnitedStates()
+for period in period_array:
+    modified_date = us_cal.advance(
+        start_date, period, ql.ModifiedFollowing, True)
+    maturity_date.append(modified_date)
+
+maturity_date_array = np.array(maturity_date)
+print(maturity_date_array)
+
+
+# concatenate arrays
 maturity_array = np.array(maturity_list)
 yield_array = np.array(yield_list)
 
 bond = np.column_stack((maturity_array.T, yield_array.T,
                         maturity_date_array.T))
+
 print(bond)
--- practice.py
+++ practice.py
@@ -9,6 +9,20 @@ period_array = np.array([ql.Period(1, ql.Days), ql.Period(1, ql.Months), ql.Peri
     2, ql.Years), ql.Period(3, ql.Years), ql.Period(5, ql.Years),
     ql.Period(7, ql.Years), ql.Period(7, ql.Years), ql.Period(10, ql.Years), ql.Period(20, ql.Years), ql.Period(30, ql.Years)])
 
-maturity_date = start_date + period_array
 
-print(maturity_date)
+# build calender
+maturity_date = []
+
+us_cal = ql.UnitedStates()
+for period in period_array:
+    modified_date = us_cal.advance(
+        start_date, period, ql.ModifiedFollowing, True)
+    maturity_date.append(modified_date)
+
+maturity_date_array = np.array(maturity_date)
+print(maturity_date_array)
+
+# modified_maturity = us_cal.advance(
+#    start_date, period_array, ql.ModifiedFollowing, True)
+
+# print(modified_maturity)
